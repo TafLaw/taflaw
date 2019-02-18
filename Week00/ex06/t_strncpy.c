3283c1393aec927e821cf60144332177eaa57a08
Tafadzwa <muzerenganit@gmail.com>
2019-02-18 14:16:24 +0200

    t_strncpy.c

---
+++ Week00/ex06/t_strncpy.c
@@ -0,0 +1,14 @@
+
+char	*t_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest = '\0';
+	return (dest);
+}
